c33490df7d0709878ed24b8768c7fced1e5ac495
deltaDelete <samansy@yandex.ru>
2023-10-15 18:14:14 +0700

    feat: генерация запросов с join для добавления внешних ключей
    Refs: #1

--- DbTools/Database.Asynchronous.cs
+++ DbTools/Database.Asynchronous.cs
@@ -1,8 +1,10 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
+using System.Diagnostics;
 using System.Reflection;
 using MySqlConnector;
+using SqlKata;
+using SqlKata.Compilers;
+using KeyAttribute = System.ComponentModel.DataAnnotations.KeyAttribute;
 
 namespace DbTools;
 
@@ -11,19 +13,30 @@ public partial class Database {
         await _connection.DisposeAsync();
     }
 
+    private readonly MySqlCompiler _compiler = new MySqlCompiler();
+
     public async Task<T?> GetByIdAsync<T>(int id) where T : new() {
         var columns = GetColumns<T>().ToList();
         var tableInfo = GetTableName<T>();
         var primaryKey = GetPrimaryKey<T>();
+        var foreignKeys = GetForeignKeys<T>().ToList();
 
-        await using var cmd = new MySqlCommand(
-            $"""
-            select * from `{tableInfo.Name}`
-            where @keyName = @keyValue
-            """,
-            _connection);
-        cmd.Parameters.AddWithValue("@keyName", primaryKey.ColumnAttribute.Name);
-        cmd.Parameters.AddWithValue("@keyValue", id);
+        var query = new Query(tableInfo.Name);
+        if (foreignKeys.Any()) {
+            query = foreignKeys.Aggregate(
+                query,
+                (current, fK) => current.Join(fK.ForeignKeyAttribute.TableName, fK.ForeignKeyAttribute.First,
+                                              fK.ForeignKeyAttribute.Second));
+        }
+
+        query = query
+                .Select("*")
+                .Where(primaryKey.ColumnAttribute.Name, id);
+
+        var compiled = _compiler.Compile(query);
+
+        await using var cmd = new MySqlCommand(compiled.ToString(), _connection);
+
         await using var reader = await cmd.ExecuteReaderAsync();
         while (await reader.ReadAsync()) {
             var obj = new T();
@@ -36,6 +49,12 @@ public partial class Database {
                 column.Property.SetValue(obj, reader.GetValue(column.ColumnAttribute.Name));
             }
 
+            foreach (var foreignKey in foreignKeys) {
+                var value = ResolveInnerJoin(foreignKey.PropertyInfo.PropertyType, reader,
+                                             foreignKey.ForeignKeyAttribute);
+                foreignKey.PropertyInfo.SetValue(obj, value);
+            }
+
             return obj;
         }
 
@@ -45,12 +64,25 @@ public partial class Database {
     public async IAsyncEnumerable<T> GetAsync<T>() where T : new() {
         var columns = GetColumns<T>().ToList();
         var tableInfo = GetTableName<T>();
+        var foreignKeys = GetForeignKeys<T>().ToList();
 
         if (_connection.State != ConnectionState.Open) _connection.Open();
-        await using var cmd = new MySqlCommand($"select * from `{tableInfo.Name}`", _connection);
+
+        var query = new Query(tableInfo.Name)
+            .Select("*");
+        if (foreignKeys.Any()) {
+            query = foreignKeys.Aggregate(
+                query,
+                (current, fK) => current.Join(fK.ForeignKeyAttribute.TableName, fK.ForeignKeyAttribute.First,
+                                              fK.ForeignKeyAttribute.Second));
+        }
+
+        var compiled = _compiler.Compile(query);
+
+        await using var cmd = new MySqlCommand(compiled.ToString(), _connection);
         await using var reader = await cmd.ExecuteReaderAsync();
 
-        while (reader.Read()) {
+        while (await reader.ReadAsync()) {
             var obj = new T();
             foreach (var column in columns) {
                 if (column.ColumnAttribute.Name is null) {
@@ -61,59 +93,56 @@ public partial class Database {
                 column.Property.SetValue(obj, reader.GetValue(column.ColumnAttribute.Name));
             }
 
+            foreach (var foreignKey in foreignKeys) {
+                var value = ResolveInnerJoin(foreignKey.PropertyInfo.PropertyType, reader,
+                                             foreignKey.ForeignKeyAttribute);
+                foreignKey.PropertyInfo.SetValue(obj, value);
+            }
+
             yield return obj;
         }
     }
 
     public async Task<object?> InsertAsync<T>(T obj) where T : new() {
         var columns = GetColumns<T>()
-            .Where(it => it.Property.GetCustomAttribute<KeyAttribute>() is null)
-            .ToList();
-        var columnStr = string.Join(',', columns.Select(it => it.ColumnAttribute.Name!));
-        var valuesStr = string.Join(',', columns.Select(it => '@' + it.ColumnAttribute.Name!));
+                      .Where(it => it.Property.GetCustomAttribute<KeyAttribute>() is null)
+                      .ToList();
 
         var tableInfo = GetTableName<T>();
 
         if (_connection.State != ConnectionState.Open) _connection.Open();
-        await using var cmd = new MySqlCommand(
-            $"""
-             insert into `{tableInfo.Name}`({columnStr})
-             values({valuesStr});
-             select LAST_INSERT_ID();
-             """,
-            _connection
-        );
+
+        var columnValue = new Dictionary<string, object>();
         foreach (var columnInfo in columns) {
-            cmd.Parameters.AddWithValue(columnInfo.ParameterName, columnInfo.Property.GetValue(obj));
+            columnValue.Add(columnInfo.ColumnAttribute.Name, columnInfo.Property.GetValue(obj)!);
         }
+
+        var query = new Query(tableInfo.Name)
+            .AsInsert(columnValue, true);
+        var compiled = _compiler.Compile(query);
+        Debug.WriteLine($"Executing an insert query:\n{compiled.Sql}");
+        await using var cmd = new MySqlCommand(compiled.ToString(), _connection);
 
         return await cmd.ExecuteScalarAsync();
     }
 
     public async Task UpdateAsync<T>(int id, T obj) where T : new() {
         var columns = GetColumns<T>()
-            .Where(it => it.Property.GetCustomAttribute<KeyAttribute>() is null)
-            .ToList();
+                      .Where(it => it.Property.GetCustomAttribute<KeyAttribute>() is null)
+                      .ToList();
         var keyName = GetPrimaryKey<T>().ColumnAttribute.Name;
-        var setters = string.Join(",\n",
-            columns.Select(it => $"{it.ColumnAttribute.Name} = {it.ParameterName}"));
-
         var tableInfo = GetTableName<T>();
 
         if (_connection.State != ConnectionState.Open) _connection.Open();
-        await using var cmd = new MySqlCommand(
-            $"""
-             update `{tableInfo.Name}`
-             set
-                 {setters}
-             where {keyName} = {id};
-             """
-            , _connection);
-        cmd.Parameters.AddWithValue("@keyName", keyName);
-        cmd.Parameters.AddWithValue("@keyValue", id);
-        foreach (var columnInfo in columns) {
-            cmd.Parameters.AddWithValue(columnInfo.ParameterName, columnInfo.Property.GetValue(obj));
-        }
+
+        var values =
+            columns.Select(c => new KeyValuePair<string, object>(c.ColumnAttribute.Name!, c.Property.GetValue(obj)!));
+
+        var query = new Query(tableInfo.Name)
+                    .AsUpdate(values)
+                    .Where(keyName, id);
+        var compiled = _compiler.Compile(query);
+        await using var cmd = new MySqlCommand(compiled.ToString(), _connection);
 
         await cmd.ExecuteNonQueryAsync();
     }
@@ -123,30 +152,39 @@ public partial class Database {
         var tableInfo = GetTableName<T>();
 
         if (_connection.State != ConnectionState.Open) _connection.Open();
-        await using var cmd = new MySqlCommand(
-            $"""
-             delete from `{tableInfo.Name}`
-             where @keyName = @keyValue;
-             """
-            , _connection);
-        cmd.Parameters.AddWithValue("@keyName", key.ColumnAttribute.Name);
-        cmd.Parameters.AddWithValue("@keyValue", key.Property.GetValue(obj));
+
+        var query = new Query(tableInfo.Name)
+                    .AsDelete()
+                    .Where(key.ColumnAttribute.Name, key.Property.GetValue(obj));
+        var compiled = _compiler.Compile(query);
+
+        await using var cmd = new MySqlCommand(compiled.ToString(), _connection);
 
         await cmd.ExecuteNonQueryAsync();
     }
-    
-    
+
+
     public async IAsyncEnumerable<T> GetAsync<T>(int skip, int take = 10) where T : new() {
         var columns = GetColumns<T>().ToList();
         var tableInfo = GetTableName<T>();
+        var foreignKeys = GetForeignKeys<T>();
 
         if (_connection.State != ConnectionState.Open) _connection.Open();
-        await using var cmd = new MySqlCommand(
-            $"""
-             select * from `{tableInfo.Name}`
-             limit {skip}, {take};
-             """,
-            _connection);
+
+        var query = new Query(tableInfo.Name)
+                    .Select("*")
+                    .Skip(skip)
+                    .Take(take);
+        if (foreignKeys.Any()) {
+            query = foreignKeys.Aggregate(
+                query,
+                (current, fK) => current.Join(fK.ForeignKeyAttribute.TableName, fK.ForeignKeyAttribute.First,
+                                              fK.ForeignKeyAttribute.Second));
+        }
+
+        var compiled = _compiler.Compile(query);
+
+        await using var cmd = new MySqlCommand(compiled.ToString(), _connection);
         await using var reader = await cmd.ExecuteReaderAsync();
 
         while (reader.Read()) {
@@ -159,6 +197,13 @@ public partial class Database {
 
                 column.Property.SetValue(obj, reader.GetValue(column.ColumnAttribute.Name));
             }
+
+            foreach (var foreignKey in foreignKeys) {
+                var value = ResolveInnerJoin(foreignKey.PropertyInfo.PropertyType, reader,
+                                             foreignKey.ForeignKeyAttribute);
+                foreignKey.PropertyInfo.SetValue(obj, value);
+            }
+
 
             yield return obj;
         }
